50698fab0aeb29742a73eb9c8f790d6edbfa6a9a
Amalia Hawkins <amalia.hawkins@gmail.com>
2013-10-05 10:33:38 -0400

    crumbling now working!

--- js/main.js
+++ js/main.js
@@ -71,12 +71,13 @@ function gameMouseMove(evt) {
                 redraw(middleground_ctx);
                 
                 // potential for additional scrapings
-                if (Math.random() < 0.1) {
-                    var plusone = crumble(polygons, i);
-                    console.log("ADDITIONAL SCRAPE INDEX: " + plusone);
-                    polygons[plusone].scraped = true;
-                    num_scraped++;
-                    redraw(middleground_ctx);
+                if (Math.random() < 0.5) {
+                    var plusone = crumble(i);
+                    if (plusone != -1) {
+                        polygons[plusone].scraped = true;
+                        num_scraped++;
+                        redraw(middleground_ctx);
+                    }
                 }
 
             }
@@ -197,6 +198,33 @@ function fillMiddleground(ctx) {
     };
     imageObj.src = "http://farm4.staticflickr.com/3333/3333171389_35b840e742_o.jpg";
     //imageObj.src = "http://farm3.staticflickr.com/2176/2394924890_02a6b830a7_b.jpg";
+};
 
+// contains the functionality for crumbling nearby polygons
+// in a domino effect.
+// when called, finds an unscraped polygon sharing a border point
+// with the given polygon. "polygon" arg is actually an index.
+function crumble(polygon) {
+    var access_pts = polygons[polygon]["points"];
 
+    for (var pt = 0; pt < access_pts.length; pt++) {
+        for (var pg = 0; pg < polygons.length; pg++) {
+            // if the polygon has been scraped or is the
+            // given polygon, continue.
+            if (polygons[pg]["scraped"]) continue;
+            if (pg == polygon) continue;
+
+            // find a matching point, return first possibility.
+            var poly_pts = polygons[pg]["points"];
+            for (var ppt = 0; ppt < poly_pts.length; ppt++) {   
+                if (access_pts[pt]["x"] == poly_pts[ppt]["x"] &&
+                    access_pts[pt]["y"] == poly_pts[ppt]["y"]) {
+                    // note that this returns an index
+                    // not a polygon object.
+                    return pg; 
+                }   
+            }   
+        }   
+    }   
+    return -1;
 };
