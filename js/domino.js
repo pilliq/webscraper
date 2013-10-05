42db4e16e2323642d5ff9d9c41e17824287fa98c
Amalia Hawkins <amalia.hawkins@gmail.com>
2013-10-05 01:25:38 -0400

    contains the basic crumbling functionality

---
+++ js/domino.js
@@ -0,0 +1,27 @@
+// contains the functionality for crumbling nearby polygons
+// in a domino effect.
+
+// when called, finds an unscraped polygon sharing a border point
+// with the given polygon.
+function crumble(polygons, polygon) {
+    var access_pts = polygon["points"];
+
+    for (var pt = 0; pt < access_pts.length; pt++) {
+        for (var pg = 0; pg < polygons.length; pg++) {
+            // if the polygon has been scraped or is the
+            // given polygon, continue.
+            if (polygons[pg]["scraped"]) continue;
+            if (polygons[pg] == polygon) continue;
+
+            // find a matching point, return first possibility.
+            var poly_pts = polygons[pg];
+            for (var ppt = 0; ppt < poly_pts.length; ppt++) {
+                if (access_pts[pt]["x"] == poly_pts[ppt]["x"] &&
+                    access_pts[pt]["y"] == poly_pts[ppt]["y"]) {
+                    return polygons[pg];
+                }
+            }
+        }
+    } 
+}
+
